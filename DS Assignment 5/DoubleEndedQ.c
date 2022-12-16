468e0137d9e03678317524c89e4c1df2fb484359
Joel Antony <joel.geoanto@gmail.com>
2022-12-16 20:41:05 +0530

    Some more changes

--- DS Assignment 5/DoubleEndedQ.c
+++ DS Assignment 5/DoubleEndedQ.c
@@ -2,24 +2,22 @@
 #include <stdlib.h>
 
 struct Node{
-    struct Node* prev;
     int data;
-    struct Node* next
+    struct Node* next;
 };
 
-struct Node* head=NULL;
-struct Node* tail=NULL;
+struct Node* front=NULL;
+struct Node* rear=NULL;
 
 void IsEmpty(int elem){ //DRY---->Don't Repeat Yourself
 
     struct Node* newNode=malloc(sizeof(struct Node));
 
-    head=newNode;
-    tail=newNode;
+    front=newNode;
+    rear=newNode;
 
-    head->data=elem;
-    head->next=NULL;
-    head->prev=NULL;
+    front->data=elem;
+    front->next=NULL;
 
 }
 
@@ -27,19 +25,18 @@ void EnqueueFront(int elem){
 
     struct Node* newNode=malloc(sizeof(struct Node));
 
-    if (head==NULL){ //Queue Empty
+    if (front==NULL){ //Queue Empty
         IsEmpty(elem);
     }
 
     else{
 
-        tail->next=newNode; //Updating the 'next' part of the previous Node before updating tail
-        newNode->prev=tail; //Updating the 'prev' of lastest Node 
+        rear->next=newNode; //Updating the 'next' part of the previous Node before updating rear
 
-        tail=newNode; //Tail is now the new Node (No Shit Sherlock)
+        rear=newNode; //rear is now the new Node (No Shit Sherlock)
 
-        tail->data=elem;
-        tail->next=NULL;
+        rear->data=elem;
+        rear->next=NULL;
 
     }
 
@@ -49,22 +46,182 @@ void EnqueueBack(int elem){
 
     struct Node* newNode=malloc(sizeof(struct Node));
 
-    if (head==NULL){ //Queue Empty
+    if (front==NULL){ //Queue Empty
         IsEmpty(elem);
     }
 
     else{
-        head->prev=newNode;
-        newNode->next=head;
-
-        head=newNode;
-
-        head->data=elem;
-        head->prev=NULL;
+        newNode->next=front;
+        front=newNode;
+        front->data=elem;
+        
     }
 
 }
 
-void DeQueue(){
+void DeQueueBack(){
+
+    struct Node* temp;
+    struct Node* ptr;
+
+    ptr=front;
+
+    while(ptr->next!=rear){
+        ptr=ptr->next;
+    }
+
+    rear=ptr;
+    temp=ptr->next;
+
+    rear->next=NULL;
+
+    free(temp);
+    temp=NULL;
+  
+}
+
+void DeQueueFront(){
     
+    struct Node* ptr;
+
+    if (front==NULL){
+        printf("Queue empty");
+    }
+
+    else{
+
+        if (front==rear && rear!=NULL){
+
+            free(front);
+            front=NULL;
+            rear=NULL;
+        }
+
+        else{
+
+            ptr=front;
+            front=front->next;
+
+            ptr->next=NULL;
+            free(ptr);
+            ptr=NULL;
+        }
+    }   
+
+}
+
+void Display(){
+
+    struct Node* ptr;
+    ptr=front;
+
+    if (front==NULL){
+        printf("\nThe Queue is empty\n");
+    }
+
+    else{
+
+        while(ptr!=NULL){
+            printf("%d ",ptr->data);
+            ptr=ptr->next;
+        }
+    }
+
+}
+
+
+int main(){
+
+    int choice;
+    int elem;
+
+    printf("Double Ended Queue");
+
+    do{
+
+        printf("\n1.Enqueue");
+        printf("\n2.Dequeue ");
+        printf("\n3.Display");
+        printf("\n4.Exit\n\n");
+
+        printf("Enter your choice: ");
+        scanf("%d",&choice);
+
+        switch(choice){
+
+            case 1:
+
+                int c1;
+
+                printf("\n1.Insert at front");
+                printf("\n2.Insert at back\n\n");
+
+                printf("Enter the choice: ");
+                scanf("%d",&c1);
+
+                switch(c1){
+
+                    case 1:
+
+                        printf("\nEnter the element: ");
+                        scanf("%d",&elem);
+
+                        EnqueueFront(elem);
+                        break;
+                    
+                    case 2:
+                        printf("\nEnter the element: ");
+                        scanf("%d",&elem);
+
+                        EnqueueBack(elem);
+                        break;
+
+                    default:
+                        printf("\nInvalid choice!\n");
+
+                }
+
+                break;
+            
+            case 2:
+                int c2;
+
+                printf("\n1.Delete from front");
+                printf("\n2.Delete from back\n\n");
+
+                printf("Enter the choice: ");
+                scanf("%d",&c2);
+
+                switch(c2){
+
+                    case 1:
+                        DeQueueFront();
+                        break;
+
+                    case 2:
+                        DeQueueBack();
+                        break;
+
+                    default:
+                        printf("\nInvalid Choice! ");
+
+                }
+
+                break;
+            
+            case 3:
+                Display();
+                break;
+            
+            case 4:
+                printf("");
+                break;
+
+            default:
+                printf("\nInvaild Choice!");
+
+        }
+
+    }while(choice!=4);
+
 }
